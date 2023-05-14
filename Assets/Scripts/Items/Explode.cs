562e64936650f4263950ef7186a8b5465a8fb00d
szbanek <stefan.zbaszyniak.gim4@gmail.com>
2023-05-14 15:37:44 +0200

    add explosion sound

--- Assets/Scripts/Items/Explode.cs
+++ Assets/Scripts/Items/Explode.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(AudioSource))]
 public class Explode : MonoBehaviour
 {
     private Animator animator;
     private SpriteRenderer spriteRenderer;
+    private AudioSource audioSource;
     private void Start()
     {
         animator = GetComponent<Animator>();
         animator.enabled = false;
         spriteRenderer = GetComponent<SpriteRenderer>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void StartExplode()
@@ -19,6 +22,7 @@ public class Explode : MonoBehaviour
         animator.enabled = true;
         spriteRenderer.size = new Vector2(5, 5);
         animator.Play("BaseLayer.Explode");
+        AudioSource.PlayClipAtPoint(audioSource.clip, transform.position);
     }
 
     public void EndExplode()
